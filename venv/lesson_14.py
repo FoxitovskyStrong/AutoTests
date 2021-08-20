09d117cf82106779b87bd0cc8ef132579ece5cd4
Dmitry Poshekhontsev <dmitry.poshekhontsev@protobase.ru>
2021-08-20 22:56:23 +0300

    Полезная explicit_wait2(ожидания)

---
+++ venv/lesson_14.py
@@ -0,0 +1,26 @@
+import math
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def calc(x):
+	return str(math.log(abs(12 * math.sin(int(x)))))
+
+link = 'http://suninjuly.github.io/explicit_wait2.html'
+
+try:
+	driver = webdriver.Chrome()
+	driver.get(link)
+	price = WebDriverWait(driver, 12).until(EC.text_to_be_present_in_element((By.ID, 'price'), '$100'))
+	book = driver.find_element_by_id('book').click()
+	input_value = driver.find_element_by_id('input_value')
+	x = input_value.text
+	math_x = calc(x)
+	answer = driver.find_element_by_id('answer').send_keys(math_x)
+	solve = driver.find_element_by_id('solve').click()
+finally:
+	time.sleep(5)
+	driver.close()
+	driver.quit()
