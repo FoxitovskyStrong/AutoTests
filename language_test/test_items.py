5fc1fa4d85efb8e7ececf2ccb92973ba7c3d8e0a
d.poshekhontsev <d.poshekhontsev@UNISTREAM.COM>
2021-08-25 09:51:40 +0300

    проверка кнопки и языка

--- language_test/test_items.py
+++ language_test/test_items.py
@@ -7,7 +7,7 @@ import pytest
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 class TestLanguage():
-    def test_language(self, driver):
+    def test_language_and_botton_shop(self, driver):
         driver.get(link)
         button = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#add_to_basket_form > button')))
         button = len(driver.find_elements_by_css_selector('#add_to_basket_form > button'))
